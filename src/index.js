b1eaa2c88bc85da3a62ab2485f85316dbd9e5771
Abhishek Arif <abhishek@Abhisheks-MacBook-Air.local>
2022-09-01 12:48:44 +0530

    first

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+function generateCodeFromObject(obj){
+    //return a code generated string
+    if((a.top > b.top) && (a.bottom > b.bottom) && (a.left > b.left) && (a.right > b.rioht))
+    {
+    console.log("Completely Contained");
+   }
+    else
+    {
+        console.log("Totally Overlapping");
+    }
+   
+module.exports=generateCodeFromObject;
+
+
+// ReactDOM.render(<App />, document.getElementById("root"));
